ff5ac065e2de80245c3a00f47521895a1dfcbeef
Azamat <52107423+Azamat-afzalov@users.noreply.github.com>
2025-02-15 17:54:00 +0100

    Config

--- src/cluster_B.sh
+++ src/cluster_B.sh
@@ -1,33 +1,52 @@
 #!/usr/bin/env bash
 #SBATCH --partition=all
-#SBATCH --ntasks=361
-#SBATCH --job-name=my_job_name
-#SBATCH --output=test_result_%A_%a.log
-#SBATCH --time=00:10:00
-#SBATCH --array=1-10
+#SBATCH --ntasks=64
+#SBATCH --job-name=my_job_name                                                                                          
+#SBATCH --output=test_result.log                                                                                        
+#SBATCH --time=00:10:00                                                                                                                                                                                                                                                                                                                                                 
+SOURCE_FILE="mpi_taskB.c"                                                                                                                                                                                                                          
+EXECUTABLE="mpi_taskB"
 
-SOURCE_FILE="mpi_taskB.c"
+echo "(800 - 64): \n"
+mpicc -o $EXECUTABLE -D MATRIX_SIZE=800 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+mpirun -np 64 ./$EXECUTABLE
+echo "\n"
+# echo "(800 - 361): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=800 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 361 ./$EXECUTABLE
+# echo "\n"
+# echo "(2000 - 64): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=2000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 64 ./$EXECUTABLE
+# echo "\n"
+# echo "(2000 - 361): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=2000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 361 ./$EXECUTABLE
+# echo "\n"
+# echo "(4000 - 64): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=4000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 64 ./$EXECUTABLE
+# echo "\n"
+# echo "(4000 - 361): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=4000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 361 ./$EXECUTABLE
+# echo "\n"
+# echo "(6000 - 64): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=6000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 64 ./$EXECUTABLE
+# echo "\n"
+# echo "(6000 - 361): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=6000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 361 ./$EXECUTABLE
+# echo "\n"
 
-# Define an array of configurations
-CONFIGS=(
-    "800 64"
-    "800 361"
-    "2000 64"
-    "2000 361"
-    "4000 64"
-    "4000 361"
-    "6000 64"
-    "6000 361"
-    "8000 64"
-    "8000 361"
-)
+# echo "(8000 - 64): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=8000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 64 ./$EXECUTABLE
+# echo "\n"
+# echo "(8000 - 361): \n"
+# mpicc -o $EXECUTABLE -D MATRIX_SIZE=8000 -D PRINT=false $SOURCE_FILE                                                                                                                                                                                 
+# mpirun -np 361 ./$EXECUTABLE
+# echo "\n"
 
-# Extract MATRIX_SIZE and NUM_PROCESSES from the array using SLURM_ARRAY_TASK_ID
-CONFIG="${CONFIGS[$SLURM_ARRAY_TASK_ID - 1]}"
-read MATRIX_SIZE NUM_PROCESSES <<< "$CONFIG"
 
-EXECUTABLE="mpi_taskB_${MATRIX_SIZE}_${NUM_PROCESSES}"
-
-echo "Running (MATRIX_SIZE=$MATRIX_SIZE - np=$NUM_PROCESSES)"
-mpicc -o $EXECUTABLE -D MATRIX_SIZE=$MATRIX_SIZE -D PRINT=false $SOURCE_FILE
-mpirun -np $NUM_PROCESSES ./$EXECUTABLE
