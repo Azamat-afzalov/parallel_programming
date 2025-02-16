f8c45ec94076ecdc98b5d1ae0909fa63ba5e4767
Azamat <52107423+Azamat-afzalov@users.noreply.github.com>
2025-02-16 19:31:31 +0100

    changes

--- src/cluster_sanish.sh
+++ src/cluster_sanish.sh
@@ -8,16 +8,25 @@
 SOURCE_FILE="mpi_sanish_taskB.c"
 
 # Define an array of MATRIX_SIZE values
-MATRIX_SIZES=(800 2000 4000 6000 8000)
-NUM_PROCESSES=(64 361)
+# MATRIX_SIZES=(800 2000 4000 6000 8000)
+# NUM_PROCESSES=(64 361)
 
-for SIZE in "${MATRIX_SIZES[@]}"; do
-    for NP in "${NUM_PROCESSES[@]}"; do
-        EXECUTABLE="mpi_sanish_taskB_${SIZE}_${NP}"
+# for SIZE in "${MATRIX_SIZES[@]}"; do
+#     for NP in "${NUM_PROCESSES[@]}"; do
+#         EXECUTABLE="mpi_sanish_taskB_${SIZE}_${NP}"
         
-        echo "Running (MATRIX_SIZE=$SIZE - np=$NP)"
-        mpicc -o $EXECUTABLE -D MATRIX_SIZE=$SIZE $SOURCE_FILE 
-        mpirun -np $NP ./$EXECUTABLE
-        echo "\n"
-    done
-done
+#         echo "Running (MATRIX_SIZE=$SIZE - np=$NP)"
+#         mpicc -o $EXECUTABLE -D MATRIX_SIZE=$SIZE $SOURCE_FILE 
+#         mpirun -np $NP ./$EXECUTABLE
+#         echo "\n"
+#     done
+# done
+
+EXECUTABLE="mpi_sanish_taskB"
+SIZE=8000
+NP=361
+
+echo "Running (MATRIX_SIZE=8000 - np=361)"
+mpicc -o $EXECUTABLE -D MATRIX_SIZE=$SIZE $SOURCE_FILE 
+mpirun -np $NP ./$EXECUTABLE
+echo "\n"
